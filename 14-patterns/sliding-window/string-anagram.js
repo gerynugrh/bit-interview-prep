30191859b57af2cdc7446cf059c3772bfe537c45
Gery Wahyu <gerywahyunugraha@gmail.com>
2020-03-09 17:24:55 +0700

    Clarify notes

--- 14-patterns/sliding-window/string-anagram.js
+++ 14-patterns/sliding-window/string-anagram.js
@@ -1,10 +1,12 @@
 /*
 
-Given a string s and a non-empty string p, 
-find all the start indices of p's anagrams in s.
+	Given a string s and a non-empty string p, 
+	find all the start indices of p's anagrams in s.
 
-Strings consists of lowercase English letters only 
-and the length of both strings s and p will not be larger than 20,100.
+	Strings consists of lowercase English letters only 
+	and the length of both strings s and p will not be larger than 20,100.
+
+	Status: TESTED on LeetCode
 
 */
 
